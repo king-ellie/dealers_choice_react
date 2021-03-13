547a3645d4c4c90d125d085137d26d7f7c81f8b5
Ellie King <elliebking1@gmail.com>
2021-03-12 20:49:52 -0500

    webpack successfully compiling and communicating

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
-app.use('/src', express.static(path.join(__dirname, 'src')));
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
 
 app.get('/', (req, res, next) => {
     res.sendFile(path.join(__dirname, 'client/index.html'))
